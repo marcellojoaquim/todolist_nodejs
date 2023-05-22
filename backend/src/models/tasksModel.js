e692ab98e80cb3560065dd2b7f046dd95ec16f79
marcellojoaquim <marcellojoaquim1@hotmail.com>
2023-05-21 21:41:31 -0300

    finalizando rota get

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -2,7 +2,7 @@ const connection = require('./connection');
 
 
 const getAll = async ()=> {
-    const tasks = await connection.execute('SELECT * FROM tasks');
+    const [tasks] = await connection.execute('SELECT * FROM tasks');
     return tasks;
 };
 
