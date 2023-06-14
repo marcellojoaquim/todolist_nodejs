c18fca40b4cdc352804b6f09a50b2551105a37e1
marcellojoaquim <marcellojoaquim1@hotmail.com>
2023-06-14 00:24:58 -0300

    inclusão da rota post e de middleware de validação

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,12 +1,19 @@
 const tasksModel = require('../models/tasksModel');
 
-const getAll =  async (request, response) =>{
+const getAll =  async (_request, response) =>{
     const tasks = await tasksModel.getAll();
 
     return response.status(200).json(tasks);
 };
 
+const createTask = async (req, res)=> {
+    
+    const createdTask = await tasksModel.createTask(req.body);
+
+    return res.status(201).json(createdTask);
+};
 
 module.exports = {
-    getAll
+    getAll,
+    createTask
 };
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const tasksController = require('./controllers/tasksController');
-
 const router = express.Router();
+const tasksMiddleware = require('./middlewares/tasksMiddleware');
 
 router.get('/tasks', tasksController.getAll);
+router.post('/tasks', tasksMiddleware.validateBody, tasksController.createTask);
+
 
 
 
